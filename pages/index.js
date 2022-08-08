4df80b87cd2ad72cf6c6fa78c1c7a7b5a5287b9c
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2022-08-08 16:21:05 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className='bg-red-600'>
+      <Head>
+        <title>Country API</title>
+        <meta name="description" content="Frontend Mentor Country API co" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+
+     
+    </div>
+  )
+}
